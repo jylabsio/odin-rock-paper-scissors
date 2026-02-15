5ba6001240e3be825c7ccdc8e6561e6c544ae350
jylabsio <jylabs.io@gmail.com>
2026-02-15 16:54:35 -0600

    V1.2 : Human Choice : Pop Up Box

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+// Get Computer Choice
+
 function getComputerChoice() {
 
     let randomNumber = Math.floor(Math.random() * 3);
@@ -18,3 +20,14 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
+
+
+
+// Get Human Choice : Pop Up Box
+
+function getHumanChoice() {
+return prompt("Choose rock, paper, or scissors:")
+
+}
+
+console.log(getHumanChoice());
