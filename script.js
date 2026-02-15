ccc6210ffff260d000e5b9167969ccfb99a3af1c
jylabsio <jylabs.io@gmail.com>
2026-02-15 16:06:12 -0600

    V1.1 : Computer Choice

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    }
+    else if (randomNumber === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+
+}
+
+console.log(getComputerChoice());
+
